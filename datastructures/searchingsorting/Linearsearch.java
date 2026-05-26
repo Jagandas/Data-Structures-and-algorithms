7e8997c39513b733cbef0b523297761f3b09e25b
Jagandas <146440024+Jagandas@users.noreply.github.com>
2026-05-26 11:49:53 +0530

    Add files via upload

---
+++ datastructures/searchingsorting/Linearsearch.java
@@ -0,0 +1,26 @@
+package Infosys.searchingsorting;
+
+public class Linearsearch {
+	public static int search(int arr[],int target) {
+		
+	for(int i=0;i<arr.length;i++) {
+		if(arr[i]==target) {
+			return i;
+		}
+	}
+	return -1;
+}
+	public static void main(String []args) {
+		int arr[]= {10,20,30,40,50};
+		int target=30;
+		int result=search(arr,target);
+		
+		if(result!=-1) {
+			System.out.println("element found at index:" +result);
+			
+		}
+		else {
+			System.out.println("element not found");
+		}
+	}
+}
